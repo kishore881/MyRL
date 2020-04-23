fd05be8b759e28bc0275281d2349f0933442ba44
kancherlakishorereddy <kishorereddykancherla@gmail.com>
2020-04-23 21:43:10 +0530

    little tweaks in front-end

--- views/main.js
+++ views/main.js
@@ -21,38 +21,37 @@ $("#url-form").submit(function (event) {
 
 function success(newUrl) {
   var content = `<div class="container jumbotron alert alert-primary" role="alert">
-      <a href="${newUrl}" id="foo" target="null">${newUrl}</a>
-      <button type="button" id="bar" class="float-right btn btn-outline-light btn-sm" onclick="copy('#foo')">Copy</button>
-    </div>`;
+                  <a href="${newUrl}" id="foo" target="null">${newUrl}</a>
+                  <button type="button" id="bar" class="float-right btn btn-outline-light btn-sm" onclick="copy('#foo')">Copy</button>
+                </div>`;
   $(".form-div").after(content);
 }
 
 function ctExist(oldUrl) {
   var content = `<div class="alert alert-warning alert-dismissible fade show" role="alert">
-  The custom text <strong>${oldUrl}</strong> is already taken try something else.
-  <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-    <span aria-hidden="true">&times;</span>
-  </button>
-</div>`;
+                 The custom text <strong>${oldUrl}</strong> is already taken try something else.
+                  <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                  </button>
+                </div>`;
   $(".info")[0].innerHTML = content;
 }
 
 function alExist(newUrl) {
   var content = `<div class="container jumbotron alert alert-primary" role="alert">
-  The URL was already shortened to: <br>
-  <a href="${newUrl}" id="foo" target="null">${newUrl}</a>
-  <button type="button" id="bar" class="float-right btn btn-outline-light btn-sm" onclick="copy('#foo')">Copy</button>
-</div>`;
+                  The URL was already shortened to: <br><a href="${newUrl}" id="foo" target="null">${newUrl}</a>
+                    <button type="button" id="bar" class="float-right btn btn-outline-light btn-sm" onclick="copy('#foo')">Copy</button>
+                </div>`;
   $(".form-div").after(content);
 }
 
 function reto(oldUrl) {
   var content = `<div class="alert alert-primary alert-dismissible fade show" role="alert">
-  The provided URL redirects to <a href="${oldUrl}" class="alert-link">${oldUrl}</a> 
-  <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-    <span aria-hidden="true">&times;</span>
-  </button>
-</div>`;
+                  The provided URL redirects to <a href="${oldUrl}" class="alert-link">${oldUrl}</a> 
+                  <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                  </button>
+                </div>`;
   $(".info")[0].innerHTML = content;
 }
 
